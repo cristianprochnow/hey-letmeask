d31bb30ccd74f43fd495b61a92e5de88585c83f2
Cristian Prochnow <cristianprochnow@gmail.com>
2021-06-23 15:28:45 -0300

    feat: add Home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,59 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import { FiLogIn } from 'react-icons/fi'
+
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.homePage}>
       <Head>
         <title>Início | Hey! LetMeAsk</title>
       </Head>
-
-      <h1>⚡</h1>
+      <aside className={styles.asideContent}>
+        <Image
+          src="/images/illustration.svg"
+          alt="Imagem que ilustra perguntas e respostas"
+          width="100%"
+          height="100%"
+        />
+        <h1>Toda pergunta tem uma resposta.</h1>
+        <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
+      </aside>
+      <main className={styles.mainContent}>
+        <div>
+          <Image
+            src="/images/logo.svg"
+            alt="Letmeask logo"
+            width="100%"
+            height="100%"
+          />
+          <button>
+            <Image
+              src="/images/google-icon.svg"
+              alt="Google icon"
+              width="100%"
+              height="100%"
+            />
+            Crie sua conta com o Google
+          </button>
+          <div className={styles.separator}>
+            ou entre em uma sala
+          </div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <button>
+              <FiLogIn
+                size={20}
+              />
+              Entrar na sala
+            </button>
+          </form>
+        </div>
+      </main>
     </div>
   )
 }
