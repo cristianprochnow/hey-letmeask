025592020e2bea4ab6ded3abcf0160852754a946
Cristian Prochnow <cristianprochnow@gmail.com>
2021-06-23 22:16:42 -0300

    fix(_document.js): remove wrong attribute from link tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,6 @@ export default class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Roboto:wght@400;500&display=swap"
