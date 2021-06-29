490298e00e1d972a086cf93d7689518bc49e4cdc
Cristian Prochnow <cristianprochnow@gmail.com>
2021-06-29 15:57:36 -0300

    feat: create and customize Room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,7 +1,52 @@
+import { FiCopy } from 'react-icons/fi'
+
 import '../styles/pages/Room.css'
+import logoImg from '../assets/images/logo.svg'
+import emptyQuestionsImg from '../assets/images/empty-questions.svg'
+import { Button } from '../components/Button'
 
 export function Room() {
   return (
-    <h1>Showww</h1>
+    <div className="roomPage">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Hey! LetMeAsk logo" />
+          <button>
+            <FiCopy size={20} color="#FFF" />
+            <span>
+              Sala #-Md2Sddgo8KuWinzBwa8
+            </span>
+          </button>
+        </div>
+      </header>
+
+      <main>
+        <h1>Sala React Q&A</h1>
+
+        <form>
+          <textarea
+            placeholder="O que você quer perguntar?"
+          />
+          <div className="formFooter">
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>.
+            </span>
+            <Button type="submit">
+              Enviar pergunta
+            </Button>
+          </div>
+        </form>
+
+        <span className="emptyQuestionsAdvisor">
+          <img
+            src={emptyQuestionsImg}
+            alt="There's no questions available"
+          />
+
+          <strong>Nenhuma pergunta por aqui...</strong>
+          <p>Faça o seu login e seja a primeira pessoa a fazer uma pergunta!</p>
+        </span>
+      </main>
+    </div>
   )
 }
